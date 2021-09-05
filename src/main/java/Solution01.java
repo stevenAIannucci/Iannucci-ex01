88336f803cd1526934b60ad796b6a17ecd325e4f
stevenAIannucci <stevenaiannucci@gmail.com>
2021-09-05 13:29:53 -0400

    Initial commit

---
+++ src/main/java/Solution01.java
@@ -0,0 +1,16 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 1 Solution
+ * Copyright 2021 Steven Iannucci
+ */
+
+import java.util.Scanner;
+
+public class Solution01 {
+
+    public static void main(String[ ] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name = (input.nextLine());
+        System.out.println("Hello," + name + ", nice to meet you!");
+    }
+}
